049c20c754386bf3263777cd272fa3bb2285ac7e
Huy Hoang <huy@iplayup.com>
2011-05-29 13:58:30 +1000

    Changed files permission

---
+++ Tests/GHTestCase+BDDTest.m
@@ -0,0 +1,29 @@
+//
+//  GHTestCase+BDDTest.m
+//  GHUnitIPhone
+//
+//  Created by Huy on 29/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GHTestCase+BDDTest.h"
+
+
+@implementation GHTestCase_BDDTest
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
